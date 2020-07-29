0071b9ff93f8c68aa8c056d67476f47315599962
seetharaman2009 <64462983+seetharaman2009@users.noreply.github.com>
2020-07-30 00:00:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World-ECSDEPLOY-29july2020-Devcontent-updated');
+  res.send('Hello World-ECSDEPLOY-29july2020-devhello');
 });
 
 app.listen(PORT, HOST);
