c3bf82df7b19c3f12afe50acce35ed7daec97513
Seetharaman Radhakrishnan <sraman@corp.logitech.com>
2025-04-22 11:19:27 +0530

    22apr25:server.jsupdated

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World-CICD Pipeline via GITHUB Workflow-04-april-2025');
+  res.send('Hello World-CICD Pipeline via GITHUB Workflow-22-april-2025');
 });
 
 app.listen(PORT, HOST);
